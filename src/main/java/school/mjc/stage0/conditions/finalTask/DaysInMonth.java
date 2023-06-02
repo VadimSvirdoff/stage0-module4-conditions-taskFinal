c25e3fc87a281066873102acfbed58b99f401811
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-02 15:42:57 +0300

    My first commit!

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,37 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0) {
+            System.out.println("invalid date");
+            return;
+        }
 
+        if (month < 1 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
+
+        int days;
+
+        switch (month) {
+            case 2:
+                days = isLeapYear(year) ? 29 : 28;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                days = 30;
+                break;
+            default:
+                days = 31;
+                break;
+        }
+
+        System.out.println(days);
+    }
+
+    private boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 }
